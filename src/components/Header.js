4b8b55dc6585ac51bbbbad11cf7ee8253533f4ff
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2022-01-17 18:01:32 -0500

    Add more icons to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,33 +3,66 @@ import "../css/Header.css";
 import {SiGmail} from 'react-icons/si';
 import {FaBars} from 'react-icons/fa';
 import { IconContext } from 'react-icons';
+import {IoMdSettings} from 'react-icons/io'
+import {BiHelpCircle} from 'react-icons/bi'
+import {IoMdApps} from 'react-icons/io'
+import {RiAccountCircleLine} from 'react-icons/ri'
+
 
 function Header() {
 
 
   return (
     <div className='header'>
-      {/* {left header} */}
+
+      {/* left header */}
       <div className="header__left">
-        {/* {selection} */}
+
+        {/* selection icon */}
         <IconContext.Provider value={{ className: 'selection__icon' }}>
           <FaBars />
         </IconContext.Provider>
-        {/* {gmail icon} */}
+
+        {/* gmail icon */}
         <IconContext.Provider value={{ className: 'gmail__icon' }}>
           <SiGmail />
         </IconContext.Provider>
-        {/* {Gmail} */}
-        <p>Gmail</p>
-      </div>
-      {/* {center header} */}
-      <div className="header__center">
-        <h3>center</h3>
-      </div>
-      {/* {right header} */}
-      <div className="header__right">
-        <h3>right</h3>
 
+        {/* Gmail text */}
+        <p class = "Gmail">Gmail</p>
+
+      </div>
+
+      {/* center header */}
+      <div className="header__center">
+
+        
+
+      </div>
+
+      {/* right header */}
+      <div className="header__right">
+      
+        {/* help icon */}
+        <IconContext.Provider value={{ className: 'settings__icon' }}>
+          <BiHelpCircle />
+        </IconContext.Provider>
+
+        {/* settings icon */}
+          <IconContext.Provider value={{ className: 'help__icon' }}>
+          <IoMdSettings />
+        </IconContext.Provider>
+
+        {/* apps icon */}
+        <IconContext.Provider value={{ className: 'apps__icon' }}>
+          <IoMdApps />
+        </IconContext.Provider>
+
+        {/* account icon */}
+        <IconContext.Provider value={{ className: 'account__icon' }}>
+          <RiAccountCircleLine />
+        </IconContext.Provider>
+        
       </div>
     </div>
   )
